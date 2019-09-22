61558cf33ba4b3705ef6d4141e04b557e7aafc04
Code Paradise <25129399+codersguild@users.noreply.github.com>
2019-09-22 20:07:00 +0530

    Update addtrace.cpp

--- assignment2/addtrace.cpp
+++ assignment2/addtrace.cpp
@@ -1,1 +1,104 @@
+#include <stdio.h>
+#include "pin.H"
 
+FILE * trace;
+PIN_LOCK pinLock;
+
+// This function is called before every instruction is executed
+// and prints the IP
+VOID readprintip(VOID *ip, THREADID tid, VOID *addr)
+{ 
+   PIN_GetLock(&pinLock, tid+1);
+   fprintf(trace, "%d %p R %p\n", tid, ip, addr);
+   fflush(trace);
+   PIN_ReleaseLock(&pinLock);
+}
+
+VOID writeprintip(VOID *ip, THREADID tid, VOID *addr)
+{ 
+   PIN_GetLock(&pinLock, tid+1);
+   fprintf(trace, "%d %p W %p\n", tid, ip, addr);
+   fflush(trace);
+   PIN_ReleaseLock(&pinLock);
+}
+
+
+// This routine is executed every time a thread is created.
+VOID ThreadStart(THREADID tid, CONTEXT *ctxt, INT32 flags, VOID *v)
+{
+    PIN_GetLock(&pinLock, tid+1);
+    fprintf(stdout, "thread begin %d\n",tid);
+    fflush(stdout);
+    PIN_ReleaseLock(&pinLock);
+}
+
+// This routine is executed every time a thread is destroyed.
+VOID ThreadFini(THREADID tid, const CONTEXT *ctxt, INT32 code, VOID *v)
+{
+    PIN_GetLock(&pinLock, tid+1);
+    fprintf(stdout, "thread end %d code %d\n",tid, code);
+    fflush(stdout);
+    PIN_ReleaseLock(&pinLock);
+}
+
+// Pin calls this function every time a new instruction is encountered
+VOID Instruction(INS ins, VOID *v)
+{
+    UINT32 memOperands = INS_MemoryOperandCount(ins);
+
+    for (UINT32 memOp = 0; memOp < memOperands; memOp++) {
+        if (INS_MemoryOperandIsRead(ins, memOp)) {
+            INS_InsertPredicatedCall(ins, IPOINT_BEFORE, (AFUNPTR)readprintip, IARG_INST_PTR, IARG_THREAD_ID, IARG_MEMORYOP_EA, memOp, IARG_END);
+        }
+        if (INS_MemoryOperandIsWritten(ins, memOp)) {
+            INS_InsertPredicatedCall(ins, IPOINT_BEFORE, (AFUNPTR)writeprintip, IARG_INST_PTR, IARG_THREAD_ID, IARG_MEMORYOP_EA, memOp, IARG_END);
+        }
+    }
+
+}
+
+// This function is called when the application exits
+VOID Fini(INT32 code, VOID *v)
+{
+    fprintf(trace, "#eof\n");
+    fclose(trace);
+}
+
+/* ===================================================================== */
+/* Print Help Message                                                    */
+/* ===================================================================== */
+
+INT32 Usage()
+{
+    PIN_ERROR("This Pintool prints the IPs of every instruction executed\n" 
+              + KNOB_BASE::StringKnobSummary() + "\n");
+    return -1;
+}
+
+/* ===================================================================== */
+/* Main                                                                  */
+/* ===================================================================== */
+
+int main(int argc, char * argv[])
+{
+    trace = fopen("addtrace.out", "w");
+    
+    PIN_InitLock(&pinLock);
+
+    // Initialize pin
+    if (PIN_Init(argc, argv)) return Usage();
+
+    // Register Instruction to be called to instrument instructions
+    INS_AddInstrumentFunction(Instruction, 0);
+
+    PIN_AddThreadStartFunction(ThreadStart, 0);
+    PIN_AddThreadFiniFunction(ThreadFini, 0);
+
+    // Register Fini to be called when the application exits
+    PIN_AddFiniFunction(Fini, 0);
+    
+    // Start the program, never returns
+    PIN_StartProgram();
+    
+    return 0;
+}
